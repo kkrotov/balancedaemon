d9305d922729affad5f88f6cec9080acb5570ed6
Konstantin Krotov <kkrotov@rambler.ru>
2020-02-27 12:33:59 +0300

    dbuser

--- install_db.sh
+++ install_db.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-psql template1 -c "create database $1"
+psql template1 -U $2 -c "create database $1"
 if [ $? -ne 0 ]
 then
 	echo "Error creating database $1"
 	exit 1
 fi
-psql $1 < balance.sql
+psql $1 -U $2 < balance.sql
 if [ $? -ne 0 ]
 then
 	echo "Error restoring database dump"
 	exit 1
 fi
-psql $1 -c "ALTER FUNCTION public.decrement_balance(name text, val money) OWNER TO $2;" 
-psql $1 -c "ALTER FUNCTION public.increment_balance(name text, val money) OWNER TO $2;" 
-psql $1 -c "ALTER FUNCTION public.log_balance_changes() OWNER TO $2;" 
-psql $1 -c "ALTER TABLE public.operationlog OWNER TO $2;" 
-psql $1 -c "ALTER TABLE public.userbalance OWNER TO $2;" 
-psql $1 -c "ALTER TABLE public.userbalance_id_seq OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER FUNCTION public.decrement_balance(name text, val money) OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER FUNCTION public.increment_balance(name text, val money) OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER FUNCTION public.log_balance_changes() OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER TABLE public.operationlog OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER TABLE public.userbalance OWNER TO $2;" 
+psql $1 -U $2 -c "ALTER TABLE public.userbalance_id_seq OWNER TO $2;" 
 
