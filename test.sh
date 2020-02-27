5980575dd7a91575826c9888be3055f0df4ba820
Konstantin Krotov <kkrotov@rambler.ru>
2020-02-27 12:14:48 +0300

    dbname

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting daemon ..."
-./balancedaemon 8888 dbname=$1 
+./balancedaemon 8888 "dbname=$1 user=$2" 
 if [ $? -ne 0 ]
 then
 	echo "Error starting daemon"
@@ -9,12 +9,27 @@ fi
 
 echo "Testing add balance operation ..."
 RES=`echo "add ivanov 10" | netcat localhost 8888`
+if [ ! -n "${RES}" ]
+then
+	echo "Cannot receive responce from balance daemon"
+	exit 1
+fi
 echo ${RES}
 
 echo "Testing subtract balance operation ..."
 RES=`echo "sub ivanov 10" | netcat localhost 8888`
+if [ ! -n "${RES}" ]
+then
+	echo "Cannot receive responce from balance daemon"
+	exit 1
+fi
 echo $RES
 
 echo "Finishing daemon process ..."
 RES=`echo quit | netcat localhost 8888`
+if [ ! -n "${RES}" ]
+then
+	echo "Cannot receive responce from balance daemon"
+	exit 1
+fi
 echo $RES
